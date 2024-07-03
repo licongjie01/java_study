3fdb3c43041283c40eef7f4175c2534fd1975484
licongjie22 <liconhjie22@gmail.com>
2024-07-03 20:03:11 +0800

    array

--- java/array/src/main/java/org/example/array1/ArrayTest.java
+++ java/array/src/main/java/org/example/array1/ArrayTest.java
@@ -5,33 +5,36 @@ public class ArrayTest {
         //数组的声明和初始化
 
         //声明数组 ageArray
-        int [] ageArray;
+        Integer[] ageArray;
         //定义数组ageArray 并写入三个值；数组长度为3 下标从0开始
-        ageArray = new int []{10,20,30};
+        ageArray = new Integer[]{10, 20, 30};
         String[] nameArray;
         nameArray = new String[3];//数组长度3
         nameArray[0] = "张三";
         nameArray[1] = "李四";
         nameArray[2] = "王五";
+
+        Student [] studentArray = new Student[]{new Student("zhangshan",10)
+        ,new Student("lisi",20)
+        ,new Student("wangwu",30)};
+
+        printArray(ageArray);
+        printArray(nameArray);
+        printArray(studentArray);
+
     }
-    class Student{
-       private String name;
-        private int age;
-
-        public String getName() {
-            return name;
+    public static void printArray(Object [] objects){
+        if (objects instanceof Integer[] || objects instanceof String[]) {
+            for (Object object : objects) {
+                System.out.print(object + "\t");
+            }
+        } else {
+            for (Object object : objects) {
+                System.out.print(object.toString() + "\t");
+            }
         }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
+        System.out.print("\n");
     }
+
+
 }
